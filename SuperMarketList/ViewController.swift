7d1ea6b48c5eaca90dc407371565f4ebef2e505f
Xiomara Chunga Carrillo <geraldyne.110101@gmail.com>
2022-09-09 15:48:13 -0500

    Initial Commit

---
+++ SuperMarketList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperMarketList
+//
+//  Created by Xio iOS on 9/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
